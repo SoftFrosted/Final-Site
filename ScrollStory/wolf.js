62ef1ae4c73db50da5f67e206819511fc9803eaf
SoftFrosted <adamkahlke@gmail.com>
2024-12-10 13:10:45 -0500

    Please update oml

--- ScrollStory/wolf.js
+++ ScrollStory/wolf.js
@@ -1,16 +1,19 @@
 let lastScrollTop = 0;
+let degreeChange = 0;
 
 window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollIncrement = 2;
+    degreeChange += (scrollTop > lastScrollTop) ? scrollIncrement : -scrollIncrement;
 
-    if (scrollTop > lastScrollTop) {
-        animateImages(9);
-    } else {
-        animateImages(-15);
+    if (Math.abs(degreeChange) >= 9) {
+        animateImages(degreeChange);
+        degreeChange = 0;
     }
 
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    lastScrollTop = Math.max(scrollTop, 0);
 });
+
 function animateImages(degree) {
     const topImage = document.querySelector('.top-image');
     const bottomImage = document.querySelector('.bottom-image');
@@ -18,4 +21,3 @@ function animateImages(degree) {
     topImage.style.transform = `rotate(${degree}deg)`;
     bottomImage.style.transform = `rotate(${-degree}deg)`;
 }
-
